eaf8f7bdb77fe4e000bf07a6879808a9e7cf1852
Alessandro Elias <ale.elias2011@gmail.com>
2020-04-25 20:04:17 -0300

    Script to make migrations in the database
    Signed-off-by: Alessandro Elias <ale.elias2011@gmail.com>

--- ubuntu:bionic-django/migration.sh
+++ ubuntu:bionic-django/migration.sh
@@ -1,13 +1,32 @@
 #!/bin/sh
 
-set -ex
+set -e
 
-su - app <<EOF
-source $HOME/venv/bin/activate
-cd blog/src
-python manage.py migrate
+perr() {
+	echo "$@" 1>&2
+}
+
+run_as_root() {
+    su - app <<EOM
+. \$HOME/.venv/bin/activate
+cd \$HOME/blog/src
+python manage.py migrate \
 && echo -e "from django.contrib.contenttypes.models import ContentType\nContentType.objects.all().delete()\n" | python manage.py shell \
 && python manage.py loaddata db.json
-EOF
+EOM
 
-exit 0
+    exit $?
+}
+
+UID=`id -u`
+if [ $UID -eq 0 ]; then
+    run_as_root
+fi
+
+. $HOME/.venv/bin/activate
+cd $HOME/blog/src
+python manage.py migrate \
+&& echo "from django.contrib.contenttypes.models import ContentType\nContentType.objects.all().delete()\n" | python manage.py shell \
+&& python manage.py loaddata db.json
+
+exit $?
