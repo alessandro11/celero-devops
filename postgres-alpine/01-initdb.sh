40aefc2b75870c20318d461ababd7ad4f91150b7
Alessandro Elias <ale.elias2011@gmail.com>
2020-04-27 16:24:22 -0300

    Add suporte to pass password via docker env; it gives a warning if left the default
    Signed-off-by: Alessandro Elias <ale.elias2011@gmail.com>

--- postgres-alpine/01-initdb.sh
+++ postgres-alpine/01-initdb.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 set -e
 
-PASSWD=123mudar
+PASSWD=${BLOG_PASSWORD:-123mudar}
 
-echo "*** Creating user and database blog... ***"
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE blog;
-    CREATE USER blog WITH PASSWORD '$PASSWD';
-    ALTER ROLE blog SET client_encoding TO 'utf8';
-    ALTER ROLE blog SET timezone TO 'America/Sao_Paulo';
-    GRANT ALL PRIVILEGES ON DATABASE blog TO blog;
+warning_passwd() {
+    cat >&2 <<-'EOWARN'
+        *******************************************************
+        WARNING: the password for the user 'blog' has got the
+                 default pass '123mudar', CHANGE IT.
+
+        docker run ... -e BLOG_PASSWORD=<your password> ...
+        *******************************************************
+EOWARN
+}
+
+create_db() {
+    echo
+    echo "*** Creating user and database 'blog'... ***"
+    echo
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+         CREATE DATABASE blog;
+         CREATE USER blog WITH PASSWORD '$PASSWD';
+         ALTER ROLE blog SET client_encoding TO 'utf8';
+         ALTER ROLE blog SET timezone TO 'America/Sao_Paulo';
+         GRANT ALL PRIVILEGES ON DATABASE blog TO blog;
 EOSQL
+}
+
+[ -v $BLOG_PASSWORD ] &&  warning_passwd
+create_db
+
+exit 0
