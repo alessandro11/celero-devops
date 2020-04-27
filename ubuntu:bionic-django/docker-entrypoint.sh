01a141ec35243919b17a59cb5c46c19f35607b31
Alessandro Elias <ale.elias2011@gmail.com>
2020-04-27 09:56:46 -0300

    Setup database settings via env from docker cmd line; default values db: blog, user: blog, pass: 123mudar, server: localhost
    Signed-off-by: Alessandro Elias <ale.elias2011@gmail.com>

--- ubuntu:bionic-django/docker-entrypoint.sh
+++ ubuntu:bionic-django/docker-entrypoint.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 set -e
 
+DBNAME=${DB_NAME:-blog}
+DBUSER=${DB_USER:-blog}
+DBPASSWD=${DB_PASSWD:-123mudar}
+DBSERVER=${DB_SERVER:-localhost}
+
 run_migrations() {
     /home/app/migration.sh
 }
 
+db_settings() {
+    sed -i "93s/DBNAME/$DBNAME/" /home/app/blog/src/blog/settings.py
+    sed -i "94s/LOGIN/$DBUSER/" /home/app/blog/src/blog/settings.py
+    sed -i "95s/PASSWD/$DBPASSWD/" /home/app/blog/src/blog/settings.py
+    sed -i "96s/DBSERVER/$DBSERVER/" /home/app/blog/src/blog/settings.py
+}
+
+
 if [ "$1" = 'runserver' ]; then
+    db_settings
     run_migrations
 
     shift
